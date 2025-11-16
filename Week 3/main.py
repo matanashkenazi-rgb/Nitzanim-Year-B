c69856cbff5b028f528c3482dde84b2dba81d0f5
Matan <mataniwani1999@gmail.com>
2025-11-16 09:53:27 +0200

    Added functions from class

--- Week 3/main.py
+++ Week 3/main.py
@@ -1,6 +1,11 @@
 #Class 3
 
+
 #Set
+
+
+
+
 
 
 
@@ -8,8 +13,46 @@
 
 #Dictionary
 def phonebook():
-    pass
+    my_dict = {}
+    user_input = input("Enter choice: ").lower()
+    while user_input != "exit":
+        if user_input == "add":
+            name = input("Enter name to add: ")
+            number = input("Enter number to add: ")
+            my_dict[name] = number
+        elif user_input == 'find':
+            name = input("Enter name you want to find: ")
+            print(my_dict[name])
+
+        elif user_input == "remove":
+            name = input("Enter name you want to remove: ")
+            my_dict.pop(name)
+        user_input = input("Enter choice: ").lower()
+    print(my_dict)
+# phonebook()
 
 
 
+
+
+
+def shopping():
+    shopping_cart = {"milk": 10, "water": 3, "tank": 2000000}
+    for val in shopping_cart.values():
+        print(val)
+
+shopping()
+
+
+
+
+
+
+
+
+
+
+
+
+dict1= {"key1": 1, "key2":2, "key3":"hello"}
 
