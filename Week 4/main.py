424ba4479166812961d425640c4c3998e83375df
Matan <mataniwani1999@gmail.com>
2025-11-20 08:38:53 +0200

    Added playlist project

--- Week 4/main.py
+++ Week 4/main.py
@@ -33,9 +33,54 @@ liked_songs = {
         "artist": "Idan Raichel",
         "duration": (4, 45),
         "genre": "Israeli"
-    }
+    },
+    "Achshav Karov": {
+        "artist": "Idan Raichel",
+        "duration": (2, 26),
+        "genre": "Israeli"
+    },
+    "Uf Gozal": {
+            "artist": "Arik Einstein",
+            "duration": (2, 56),
+            "genre": "Israeli"
+    },
+    "Papi": {
+            "artist": "Odeya",
+            "duration": (2, 36),
+            "genre": "Pop"
+        },
 }
 
 
+def print_dict(my_dict):
+    for item in my_dict:
+        print(item)
+
+
+def maya_check(my_dict):
+    maya_song = input("Hi Maya what do you want to check? ")
+    if maya_song in my_dict:
+        maya_wants = input("Yeah that songs is in the playlist do you want to remove it? ")
+        if maya_wants.lower() == "yes":
+            liked_songs.pop(maya_song)
+    else:
+        print("No, the song doesn't exist in playlist")
+    return my_dict
+
+def maya_dont_like_artists(my_dict):
+    maya_artist = input("Hi Maya what artist do you want to remove? ")
+    for song, song_dict in list(my_dict.items()):
+        if song_dict["artist"].lower() == maya_artist.lower():
+            my_dict.pop(song)
+            print(f"Songs of {maya_artist}, removed successfully")
+
+    return my_dict
+
 def main():
-    pass
+    print_dict(liked_songs)
+    my_dict = maya_check(liked_songs)
+    my_dict = maya_dont_like_artists(my_dict)
+    print_dict(my_dict)
+
+
+main()
